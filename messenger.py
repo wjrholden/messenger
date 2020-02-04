752ab6ec5b849f910b5e0d880957d2cdb184bd1c
William Holden <wjrholden@gmail.com>
2020-02-04 10:51:37 -0700

    adds resources to messenger API

--- messenger.py
+++ messenger.py
@@ -6,7 +6,16 @@ import falcon
 class MessengerResource(object):
 
     def on_get(self, req, resp):
-        pass
+        resp.body(json.dumps{
+            "message": "GET request received!"
+        })
 
     def on_post(self, req, resp):
-        pass
+        resp.body(json.dumps{
+            "message": "POST request received!"
+        })
+
+
+app = falcon.API()
+messenger = MessengerResource()
+app.add_route('/messages', messenger)

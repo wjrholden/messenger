ce8488979c2ac383b3c9ecda3ce07a17746808b5
William Holden <wjrholden@gmail.com>
2020-02-04 10:57:38 -0700

    syntax error correction

--- messenger/messenger.py
+++ messenger/messenger.py
@@ -11,14 +11,14 @@ https://falcon.readthedocs.io/en/stable/user/quickstart.html
 class MessengerResource(object):
 
     def on_get(self, req, resp):
-        resp.body(json.dumps{
+        resp.body(json.dumps({
             "message": "GET request received!"
-        })
+        }))
 
     def on_post(self, req, resp):
-        resp.body(json.dumps{
+        resp.body(json.dumps({
             "message": "POST request received!"
-        })
+        }))
 
 
 app = falcon.API()

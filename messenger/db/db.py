ed7599c1f52a98021f295091f37785f1adbdbcb4
William Holden <wjrholden@gmail.com>
2020-02-05 10:55:14 -0700

    refactors/reorders db functions and adds DB testing entries

--- messenger/db/db.py
+++ messenger/db/db.py
@@ -17,154 +17,151 @@ def create_connection(db_file):
     try:
         conn = sqlite3.connect(db_file)
         print(f"DB connected to {db_file}")
+        conn.row_factory = sqlite3.Row
     except Error as e:
         print(f"An error occurred connecting to {db_file}: {e}")
         raise e
     return conn
 
 
-def create_user(conn, user):
-    """
-    Insert a new user in the users table
-    :param conn:
-    :param user:
-    :return: user id
-    """
-    insert_sql = ''' INSERT INTO users(name)
-                     VALUES(?) '''
-    cur = conn.cursor()
-    cur.execute(insert_sql, user)
-    conn.commit()
-    return cur.lastrowid
-
-
 def create_message(conn, msg):
     """
-    Insert a new message in the messages table
-    :param conn:
-    :param msg:
-    :return:
+    Insert a new message into the messages table
+    :param conn: the DB connection
+    :param msg: a dict of the sender ID, recipient ID, and message content
+    :return: the ID of the newly created message
     """
-    sql = ''' INSERT INTO messages(sender_id,recipient_id,content)
-              VALUES(?,?,?) '''
     cur = conn.cursor()
-    cur.execute(sql, msg)
+    cur.execute("INSERT INTO messages(sender_id,recipient_id,content) "
+                "VALUES(?,?,?)", 
+                (msg["sender_id"], msg["recipient_id"], msg["content"]))
     conn.commit()
     return cur.lastrowid
 
 
-def select_user_id_by_name(conn, user_name):
+def create_user(conn, user_name):
     """
-    Query user ID by name
-    :param conn:
-    :param user_name:
-    :return:
+    Insert a new user in the users table
+    :param conn: the DB connection
+    :param user_name: name of the user to be created
+    :return: the ID of the newly created user
     """
     cur = conn.cursor()
-    cur.execute("SELECT id FROM users WHERE name=?", (user_name,))
+    cur.execute("INSERT INTO users(name) "
+                "VALUES(?)", (user_name, ))
+    conn.commit()
+    return cur.lastrowid
 
+
+def _execute_query(conn, sql_statement, params=None):
+    """
+    Handler for all queries
+    :param conn: the DB connection
+    :param sql_statement: a SQL statement to execute
+    :param params: any parameters needed in the SQL statement
+    :return: a list of dicts with the column names and their values of a row
+    """
+    cur = conn.cursor()
+    if not params:
+        cur.execute(sql_statement)
+    else:
+        cur.execute(sql_statement, params)
     rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
-
-    return rows
+    items = [dict(row) for row in rows]
+    return items
 
 
 def get_all_messages(conn):
     """
     Query for all messages
-    :param conn:
-    :return:
+    :param conn: the DB connection
+    :return: a list of messages as dicts
     """
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM messages")
+    sql_statement = "SELECT * FROM messages"
+    messages = _execute_query(conn, sql_statement)
+    return messages
 
-    rows = cur.fetchall()
 
-    for row in rows:
-        print(row)
+def get_all_users(conn):
+    """
+    Query for all users
+    :param conn: the DB connection
+    :return: a list of users as dicts
+    """
+    sql_statement = "SELECT * FROM users"
+    users = _execute_query(conn, sql_statement)
+    return users
 
-    return rows
+
+def get_message_by_id(conn, msg_id):
+    """
+    Query for all messages
+    :param conn: the DB connection
+    :param msg_id: the ID of a message
+    :return: a message as dict
+    """
+    sql_statement = "SELECT * FROM messages WHERE id=?"
+    query_params = (msg_id, )
+    messages = _execute_query(conn, sql_statement, query_params)
+    return messages[0] if messages else None
 
 
 def select_messages_by_recipient_and_sender(conn, recipient_name, sender_name):
     """
     Query for messages to a given recipient from a given sender
-    :param conn:
+    :param conn: the DB connection
     :param recipient_name:
     :param sender_name:
     :return:
     """
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM messages "
-                "WHERE recipient_id="
-                "(SELECT id FROM users WHERE name=?) "
-                "AND sender_id="
-                "(SELECT id FROM users WHERE name=?)",
-                (recipient_name, sender_name))
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
-
-    return rows
+    sql_statement = ("SELECT * FROM messages "
+                     "WHERE recipient_id="
+                     "(SELECT id FROM users WHERE name=?) "
+                     "AND sender_id="
+                     "(SELECT id FROM users WHERE name=?)")
+    query_params = (recipient_name, sender_name)
+    messages = _execute_query(conn, sql_statement, params=query_params)
+    return messages
 
 
 def select_messages_by_recipient_name(conn, recipient_name):
     """
     Query for messages to a given recipient
-    :param conn:
+    :param conn: the DB connection
     :param recipient_name:
     :return:
     """
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM messages "
-                "WHERE recipient_id="
-                "(SELECT id FROM users WHERE name=?)",
-                (recipient_name, ))
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
-
-    return rows
+    sql_statement = ("SELECT * FROM messages "
+                     "WHERE recipient_id="
+                     "(SELECT id FROM users WHERE name=?)")
+    query_params = (recipient_name, )
+    messages = _execute_query(conn, sql_statement, params=query_params)
+    return messages
 
 
 def select_messages_by_sender_name(conn, sender_name):
     """
     Query for messages from a given sender
-    :param conn:
+    :param conn: the DB connection
     :param sender_name:
     :return:
     """
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM messages "
-                "WHERE sender_id="
-                "(SELECT id FROM users WHERE name=?)",
-                (sender_name, ))
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
-
-    return rows
+    sql_statement = ("SELECT * FROM messages "
+                     "WHERE sender_id="
+                     "(SELECT id FROM users WHERE name=?)")
+    query_params = (sender_name, )
+    messages = _execute_query(conn, sql_statement, params=query_params)
+    return messages
 
 
-def main():
-    database = "../db/messenger.db"
-    # create a database connection
-    conn = create_connection(database)
-
-    with conn:
-        get_all_messages(conn)
-
-    if conn:
-        conn.close()
-
-
-if __name__ == '__main__':
-    main()
+def select_user_id_by_name(conn, user_name):
+    """
+    Query user ID by name
+    :param conn: the DB connection
+    :param user_name: a users name (str)
+    :return: the ID of the user or none
+    """
+    sql_statement = "SELECT id FROM users WHERE name=?" 
+    query_params = (user_name, )
+    user_ids = _execute_query(conn, sql_statement, params=query_params)
+    return user_ids[0] if user_ids else None

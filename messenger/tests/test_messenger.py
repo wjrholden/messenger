352e77f7c9aecb6e42c33343f60bdc5684810c71
William Holden <wjrholden@gmail.com>
2020-02-11 09:38:48 -0700

    creates create/get single message API test

--- messenger/tests/test_messenger.py
+++ messenger/tests/test_messenger.py
@@ -1,3 +1,4 @@
+import json
 import random
 
 import requests
@@ -8,10 +9,28 @@ RANDOM_WORDS = rwg.generate_random_words(1000000)
 
 
 def test_messenger_apis():
+    """
+    tests the messenger app create and get single message functionality
+    """
     for i in range(12):
-        message = _generate_test_message()
-        print(f"message = {message}")
-    assert False
+        # create a test message through the API
+        test_message = _generate_test_message()
+        print(f"creating test message = {test_message}")
+        create = requests.post(BASE_URL, data=json.dumps(test_message))
+        assert create.status_code == 201
+
+        # get the created message and verify field matches
+        message_location = create.headers['location']
+        retrieve = requests.get(f"{BASE_URL}{message_location}")
+        assert retrieve.status_code == 200
+        returned_message = retrieve.json()
+        assert returned_message['message'] == test_message['message']
+        assert returned_message['sender'] == test_message['sender']
+        assert returned_message['recipient'] == test_message['recipient']
+
+
+def test_messenger_limit():
+    pass
 
 
 def _generate_test_message():
